98b5c0e1ff0942a745f3aa1f04ecfd93f1091edf
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-05 18:22:07 +0100

    feat: add ConsoleColors utility for ANSI color codes in hello.books package

---
+++ src/main/java/org/soujava/samples/ai/patterns/hello/books/ConsoleColors.java
@@ -0,0 +1,15 @@
+package org.soujava.samples.ai.patterns.hello.books;
+
+public class ConsoleColors {
+
+    public static final String RESET = "\033[0m";
+
+    public static final String GREEN = "\033[0;32m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+
+    private ConsoleColors() {
+    }
+}
